f6c4bb9f2953c12d57ccda4860a864015646d190
Aysel Bilmez <aybilmez@gmail.com>
2024-11-01 16:03:25 +0300

    Proje dosyası ekle.

---
+++ Week5_Pratik1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+int[] sayilar = new int[10];  // yeni bir sayilar adinda array tanimliyoruz 10 elemanli
+
+for (int i = 0; i < sayilar.Length; i++) //eleman sayisi kadar deger isticek bizden ve dolduracak array icini
+{ 
+    Console.Write("Diziye eklenecek sayıyı giriniz: ");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    sayilar[i] = sayi;
+}
+
+foreach (int sayi in sayilar) //her elemani ekrana yazdiracak foreach donguusuyle 
+{
+    Console.WriteLine(sayi);
+}
+
+Array.Resize<int>(ref sayilar, 11); //arrayi yeniden sekillendiriyoruz ve 11 elemanli yapiyoruz 
+
+Console.Write("11. elemanı giriniz: "); //11. elemani istiyor 
+sayilar[10] = Convert.ToInt32(Console.ReadLine());
+
+Array.Sort(sayilar); //sayilari siraliyor 
+
+foreach (int sayi in sayilar) //tekrardan sayilari sirasiyla yazdiriyor
+{
+    Console.WriteLine(sayi);
+}
+
+
+
+
+
+
